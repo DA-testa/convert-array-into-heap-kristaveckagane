78b84efec5ea92b961e0fa8e34b99d6871330026
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-03-13 16:41:53 +0000

    test

--- build_heap.py
+++ build_heap.py
@@ -1,9 +1,22 @@
 # python3
 
 
-def build_heap(data):
     swaps = []
-   
+    for i in range(n // 2, -1, -1):
+        while True:
+          leftchild = 2*i+1
+          rightchild = 2*i+2
+          if leftchild < n and data[leftchild] < data[root]
+           mazaks = leftchild
+          else:
+            mazaks = i
+          if rightchild < n and data[rightchild] < data[root]
+           mazaks = rightchild
+          if mazaks == i:
+            break
+          swaps.append((i,mazaks))
+          data[i], data[mazaks] == data[mazaks], data[i]
+          i = mazaks
 
     return swaps
 
@@ -13,26 +26,36 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
+    ievade = input("").strip()
+    if "f" == ievade.lower():
+        file = input("").strip()
+        read = file.readlines()
+        n = int(read[0])
+        numbers = read[1].split()
+        data = [int(x) for x in numbers]
+        swaps = build_heap(data)
+        print(len(swaps))
+        for i, j in swaps:
+          print(i, j)
+    if "i" == ievade.lower():
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+     n = int(input())
+     data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+     assert len(data) == n
 
     # calls function to assess the data 
     # and give back all swaps
-    swaps = build_heap(data)
+     swaps = build_heap(data)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
 
 
     # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
+     print(len(swaps))
+     for i, j in swaps:
         print(i, j)
 
 
